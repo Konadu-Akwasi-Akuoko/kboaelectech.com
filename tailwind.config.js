0b4a66c00e310c0cb7e7503f0ef52b36e04c4a3a
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-12-22 14:15:25 +0000

    Added header and hero section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}", "./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryWine: {
+          500: "#6B2737",
+          50: "#DFD1D4",
+          100: "#E1D4D7",
+        },
+        secondryOrange: {
+          500: "#E08E45",
+        },
+        alternateLightYellow: {
+          500: "#F5F3CB",
+        },
+      },
+    },
   },
   plugins: [],
 };
