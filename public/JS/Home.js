0fadff64c17143182d622c76b6d3f97becb75bf1
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2022-01-03 13:24:30 +0000

    Made the icons a bit smaller on small device

--- public/JS/Home.js
+++ public/JS/Home.js
@@ -3,9 +3,18 @@ let carousel_secondaryText = document.querySelector("carousel-secondaryText");
 let carousel_id = 0;
 
 let menuButton = document.querySelector("#menu-button");
+//This toggles the header gap, otherwise it'll be always visible.
 let headerGapT = document.querySelector("#header-gap-toggle");
 let headerNavigation = document.querySelector("#header-nav");
 let menuToggle = false;
+
+// Social icons
+let whatsappIcon = document.querySelector("#whatsapp-icon");
+let gmailIcon = document.querySelector("#gmail-icon");
+let callIcon = document.querySelector("#call-icon");
+let instagramIcon = document.querySelector("#instagram-icon");
+
+let windowWidth = window.innerWidth;
 
 const mainTestimonies = [
   `“KBoa Elec-Tech is a great organization. The team really informed me about the best decision for me and they went every extra mile in the installation stage!” 01`,
@@ -14,6 +23,8 @@ const mainTestimonies = [
 ];
 const nameOfTestimonials = [`Akwasi Konadu`, `Brooklyn Simmons`, `Kwame Kofi`];
 
+// When the chevron left button is pressed the carousel id is decreased
+// by one and the switch statement chooses the a testimony to display.
 function ChevronLeft() {
   carousel_id--;
   if (carousel_id === -1) {
@@ -33,6 +44,8 @@ function ChevronLeft() {
   console.log(carousel_id);
 }
 
+// When the chevron left button is pressed the carousel id is increased
+// by one and the switch statement chooses the a testimony to display.
 function ChevronRight() {
   carousel_id++;
   if (carousel_id === 3) {
@@ -52,8 +65,10 @@ function ChevronRight() {
   console.log(carousel_id);
 }
 
+// When this function is called it displays the menu or hides the menu
 function menuPressed() {
   if (menuToggle === false) {
+    // When the menu is pressed the menu button is changed to a cancel button
     menuButton.innerHTML = `<span class="iconify" data-icon="ep:close-bold" style="color: #f5f3cb;" data-width="24" data-height="24"></span>`;
     if (
       !headerGapT.classList.contains("gap-y-6") &&
@@ -64,6 +79,7 @@ function menuPressed() {
     }
     menuToggle = true;
   } else {
+    // When the cancel is pressed the cancel button is changed to a menu button
     menuButton.innerHTML = `<span class="iconify" data-icon="ant-design:menu-outlined" style="color: #f5f3cb" data-width="24" data-height="24"></span>`;
     if (
       headerGapT.classList.contains("gap-y-6") &&
@@ -73,5 +89,31 @@ function menuPressed() {
       headerNavigation.classList.add("hidden");
     }
     menuToggle = false;
+  }
+}
+
+//Change the icon sizes to 24px on small devices.
+function changeIconOnSmDevice() {
+  if (windowWidth < 640) {
+    whatsappIcon.innerHTML = `<span
+    class="iconify"
+    data-icon="akar-icons:whatsapp-fill"
+    style="color: white; font-size: 30px"
+    ></span>`;
+    gmailIcon.innerHTML = `<span
+    class="iconify"
+    data-icon="mdi:gmail"
+    style="color: white; font-size: 30px"
+    ></span>`;
+    callIcon.innerHTML = `<span
+    class="iconify"
+    data-icon="fluent:call-32-regular"
+    style="color: #fff; font-size: 30px"
+    ></span>`;
+    instagramIcon.innerHTML = `<span
+    class="iconify"
+    data-icon="ant-design:instagram-outlined"
+    style="color: #fff; font-size: 30px"
+    ></span>`;
   }
 }
